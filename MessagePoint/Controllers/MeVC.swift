41a77b603c1a46dc645e7bdde61e34ba9b53b831
Ljuba <ljmasirevic@gmail.com>
2017-12-28 23:35:21 +0100

    MeVC Added and Connected to UITabBArController

---
+++ MessagePoint/Controllers/MeVC.swift
@@ -0,0 +1,31 @@
+//
+//  MeVC.swift
+//  MessagePoint
+//
+//  Created by Ljubomir on 12/28/17.
+//  Copyright © 2017 Ljubomir. All rights reserved.
+//
+
+import UIKit
+
+class MeVC: UIViewController {
+    
+    
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var emailLbl: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+
+    @IBAction func signOutBtnWasPressed(_ sender: Any) {
+        
+    }
+    
+}
