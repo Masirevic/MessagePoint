b3e7e4f65c63d3eda70a25b4bc98008cb7fcbac0
Ljuba <ljmasirevic@gmail.com>
2017-12-29 13:20:02 +0100

    FeedCell Added

---
+++ MessagePoint/View/FeedCell.swift
@@ -0,0 +1,27 @@
+//
+//  FeedCell.swift
+//  MessagePoint
+//
+//  Created by Ljubomir on 12/29/17.
+//  Copyright © 2017 Ljubomir. All rights reserved.
+//
+
+import UIKit
+
+class FeedCell: UITableViewCell {
+
+    
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var emailLbl: UILabel!
+    @IBOutlet weak var contentLbl: UILabel!
+    
+    
+    func configureCell (profileImage: UIImage, email: String, content: String) {
+        self.profileImage.image = profileImage
+        self.emailLbl.text = email
+        self.contentLbl.text = content
+    }
+    
+    
+    
+}
